42a72199744da0b26f48368a8daa684acba52d0f
hashjaco <hashjaco@github.com>
2021-07-20 14:46:52 -0400

    refactor pets controller

--- src/controllers/pets.controller.js
+++ src/controllers/pets.controller.js
@@ -4,7 +4,6 @@ import * as _ from '../models/pets.model';
 export const getPets = async (req, res, next) => {
   try {
     const pets = await _.getAllPets()
-    console.log(pets)
     return res.status(res.statusCode).json({ status: 'success', data: pets.data || 'no data'  });
   } catch (err) {
     return res.status(err.statusCode).json({ status: 'error', data: err.message });
